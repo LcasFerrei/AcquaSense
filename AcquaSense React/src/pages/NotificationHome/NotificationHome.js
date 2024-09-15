5986fc2a7aff3e07fd2618f57a00759a8082ff23
LcasFerrei <lucasferreiraceara@gmail.com>
2024-09-15 06:03:05 -0300

    Feito a tela de notificação

--- AcquaSense React/src/pages/NotificationHome/NotificationHome.js
+++ AcquaSense React/src/pages/NotificationHome/NotificationHome.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Noti from '../../components/Notification/Notification';
 import HeaderNav from "../../components/AcquaNav/Header";
 
 function NotificationHome() {
     return (
+        <div className="notification-home">
             <HeaderNav />
+            <div className="main-content">
+                <div className="sidebar">
+                    {/* Coloque aqui os itens da sidebar se não estiverem no Header */}
+                </div>
+                <div className="noti-container">
+                    <Noti />
+                </div>
+            </div>
+        </div>
     );
 }
 
