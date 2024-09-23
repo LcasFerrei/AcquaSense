7a444d911d6a787eaf474f0ff926dfaeb595f91e
SaulSantos1 <saulsantos111@gmail.com>
2024-09-22 22:24:41 -0300

    :wrench: feat: Resolvendo conflitos do Merge

--- AcquaSense React/src/index.js
+++ AcquaSense React/src/index.js
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import React from 'react';
 
-
-// const rooot = ReactDOM.createRoot(document.getElementById('root'));
-// rooot.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-ReactDOM.render(
-  <App />,  // Remover React.StrictMode temporariamente
-  document.getElementById('root')
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <App />,  // Remover React.StrictMode temporariamente
+//   document.getElementById('root')
+// );
