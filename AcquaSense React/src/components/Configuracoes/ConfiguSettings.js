a32a7a0f4aea06423e88aadd5cc9bb5aa599c742
LeoneFernandes <141595692+LeoneFernandes@users.noreply.github.com>
2024-11-12 08:46:46 -0300

    Mudança no salvamento das opções na pagina de configurações

--- AcquaSense React/src/components/Configuracoes/ConfiguSettings.js
+++ AcquaSense React/src/components/Configuracoes/ConfiguSettings.js
@@ -1,84 +1,166 @@
-import React, { useState } from 'react';
-import './Configu.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react';
+import './Configu.css';
 
-const NotificationSettings = () => (
-  <div className="notification-settings">
-    <h2>Comunicação com o Usuário</h2>
-    <div>
-      <label>
-        <input type="checkbox" name="emailNotification" />
-        Notificar por E-mail
-      </label>
-      <label>
-        <input type="checkbox" name="appNotification" />
-        Notificar por Aplicativo
-      </label>
-    </div>
-    <div>
-      <h3>Alertas de Consumo</h3>
-      <label>
-        <input type="checkbox" name="highConsumptionAlert" />
-        Alertas de Alto Consumo
-      </label>
-      <label>
-        <input type="checkbox" name="anomalousConsumptionAlert" />
-        Alertas de Consumo Anômalo
-      </label>
-    </div>
-    <div>
-      <h3>Horário de Notificação</h3>
-      <label>
-        Horário de Notificação:
-        <input type="time" name="notificationTime" />
-      </label>
-    </div>
-  </div>
-);
+const NotificationSettings = ({ settings, setSettings }) => {
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      [name]: checked,
+    }));
+  };
 
-const DataAnalysisSettings = () => (
-  <div className="data-analysis-settings">
-    <h2>Configurações de Análise de Dados</h2>
-    <div>
-      <h3>Relatórios de Consumo</h3>
-      <label>
-        Frequência dos Relatórios:
-        <select name="reportFrequency">
-          <option value="daily">Diariamente</option>
-          <option value="weekly">Semanalmente</option>
-          <option value="monthly">Mensalmente</option>
-        </select>
-      </label>
-      <label>
-        Formato dos Relatórios:
-        <select name="reportFormat">
-          <option value="pdf">PDF</option>
-          <option value="excel">Excel</option>
-        </select>
-      </label>
+  const handleTimeChange = (e) => {
+    const { name, value } = e.target;
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="notification-settings">
+      <h2>Comunicação com o Usuário</h2>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            name="emailNotification"
+            checked={settings.emailNotification}
+            onChange={handleCheckboxChange}
+          />
+          Notificar por E-mail
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="appNotification"
+            checked={settings.appNotification}
+            onChange={handleCheckboxChange}
+          />
+          Notificar por Aplicativo
+        </label>
+      </div>
+      <div>
+        <h3>Alertas de Consumo</h3>
+        <label>
+          <input
+            type="checkbox"
+            name="highConsumptionAlert"
+            checked={settings.highConsumptionAlert}
+            onChange={handleCheckboxChange}
+          />
+          Alertas de Alto Consumo
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="anomalousConsumptionAlert"
+            checked={settings.anomalousConsumptionAlert}
+            onChange={handleCheckboxChange}
+          />
+          Alertas de Consumo Anômalo
+        </label>
+      </div>
+      <div>
+        <h3>Horário de Notificação</h3>
+        <label>
+          Horário de Notificação:
+          <input
+            type="time"
+            name="notificationTime"
+            value={settings.notificationTime || ""}
+            onChange={handleTimeChange}
+          />
+        </label>
+      </div>
     </div>
-    <div>
-      <h3>Intervalos de Dados</h3>
-      <label>
-        Intervalos de Dados:
-        <select name="dataIntervals">
-          <option value="7days">Últimos 7 dias</option>
-          <option value="30days">Últimos 30 dias</option>
-        </select>
-      </label>
+  );
+};
+
+const DataAnalysisSettings = ({ settings, setSettings }) => {
+  const handleSelectChange = (e) => {
+    const { name, value } = e.target;
+    setSettings((prevSettings) => ({
+      ...prevSettings,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="data-analysis-settings">
+      <h2>Configurações de Análise de Dados</h2>
+      <div>
+        <h3>Relatórios de Consumo</h3>
+        <label>
+          Frequência dos Relatórios:
+          <select
+            name="reportFrequency"
+            value={settings.reportFrequency}
+            onChange={handleSelectChange}
+          >
+            <option value="daily">Diariamente</option>
+            <option value="weekly">Semanalmente</option>
+            <option value="monthly">Mensalmente</option>
+          </select>
+        </label>
+        <label>
+          Formato dos Relatórios:
+          <select
+            name="reportFormat"
+            value={settings.reportFormat}
+            onChange={handleSelectChange}
+          >
+            <option value="pdf">PDF</option>
+            <option value="excel">Excel</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <h3>Intervalos de Dados</h3>
+        <label>
+          Intervalos de Dados:
+          <select
+            name="dataIntervals"
+            value={settings.dataIntervals}
+            onChange={handleSelectChange}
+          >
+            <option value="7days">Últimos 7 dias</option>
+            <option value="30days">Últimos 30 dias</option>
+          </select>
+        </label>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const ConfiguSettings = () => {
-  const [isEditing, setIsEditing] = useState(false);
+  const [settings, setSettings] = useState({
+    emailNotification: false,
+    appNotification: false,
+    highConsumptionAlert: false,
+    anomalousConsumptionAlert: false,
+    notificationTime: '', // Inicializando com valor vazio
+    reportFrequency: 'daily', // Inicializando com valor padrão
+    reportFormat: 'pdf', // Inicializando com valor padrão
+    dataIntervals: '7days', // Inicializando com valor padrão
+  });
   const [isSaved, setIsSaved] = useState(false);
 
+  // Carrega as configurações salvas do localStorage ao montar o componente
+  useEffect(() => {
+    const savedSettings = JSON.parse(localStorage.getItem('notificationSettings'));
+    if (savedSettings) {
+      setSettings(savedSettings);
+    }
+  }, []);
+
   const handleSave = () => {
-    console.log('Alterações salvas');
+    // Salva as configurações no localStorage
+    localStorage.setItem('notificationSettings', JSON.stringify(settings));
     setIsSaved(true);
-    setIsEditing(false);
     setTimeout(() => setIsSaved(false), 3000); // Mensagem de salvamento temporário
   };
 
@@ -88,16 +170,13 @@ const ConfiguSettings = () => {
         <FontAwesomeIcon icon={faCog} size="lg" className="icon" />
         <h2>Configuração</h2>
       </div>
-      <NotificationSettings />
-      <DataAnalysisSettings />
+      <NotificationSettings settings={settings} setSettings={setSettings} />
+      <DataAnalysisSettings settings={settings} setSettings={setSettings} />
       <div className="button-container">
         <div className="edit-button-container">
           <button className="edit-button" onClick={handleSave}>
             Salvar
           </button>
-          {isEditing && (
-            <button className="edit-button" onClick={() => setIsEditing(false)}>Cancelar</button>
-          )}
         </div>
       </div>
       {isSaved && <div className="save-notification">Alterações salvas com sucesso!</div>}
