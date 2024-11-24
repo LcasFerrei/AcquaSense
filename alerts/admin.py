db5dcc251d4d447b54214dd4ad945dc4eb533e9e
SaulSantos1 <saulsantos111@gmail.com>
2024-11-23 21:32:36 -0300

    Muitos Ajustes, pelo amor de deus

--- alerts/admin.py
+++ alerts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from alerts.models import AlertaConsumo
+from alerts.models import AlertaConsumo, Notificacao
 
 @admin.register(AlertaConsumo)
 class AlertaConsumoAdmin(admin.ModelAdmin):
@@ -7,3 +7,28 @@ class AlertaConsumoAdmin(admin.ModelAdmin):
     search_fields = ('mensagem', 'sensor__identificador', 'usuario__username')
     list_filter = ('tipo_alerta', 'sensor', 'usuario')
     ordering = ('-data_hora',)
+
+class NotificacaoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','mensagem', 'tipo_notificacao', 'usuario', 'data_hora', 'lida')  # Campos a serem exibidos na lista
+    list_filter = ('tipo_notificacao', 'lida')  # Filtros para a lista
+    search_fields = ('mensagem', 'usuario__username')  # Campos pesquisáveis
+    list_editable = ('lida',)  # Permite editar o campo 'lida' diretamente na lista
+    ordering = ('-data_hora',)  # Ordenar as notificações pela data, mais recentes primeiro
+
+    # Campos de filtro por data (para facilitar a busca por datas específicas)
+    date_hierarchy = 'data_hora'
+
+    # Exibição do campo 'mensagem' de forma mais legível
+    def message_truncated(self, obj):
+        return obj.mensagem[:50] + '...' if len(obj.mensagem) > 50 else obj.mensagem
+    message_truncated.short_description = 'Mensagem'
+
+    # Campos para serem exibidos no formulário de criação/edição
+    fieldsets = (
+        (None, {
+            'fields': ('titulo','mensagem', 'tipo_notificacao', 'usuario', 'lida')
+        }),
+    )
+
+# Registro do modelo no Django admin
+admin.site.register(Notificacao, NotificacaoAdmin)
--- alerts/models.py
+++ alerts/models.py
@@ -16,3 +16,24 @@ class AlertaConsumo(models.Model):
 
     def __str__(self):
         return f"Alerta {self.get_tipo_alerta_display()} em {self.data_hora}"
+
+class Notificacao(models.Model):
+    TIPOS_NOTIFICACAO = [
+        ('INFO', 'Informação'),
+        ('ALERTA', 'Alerta'),
+        ('ERRO', 'Erro'),
+    ]
+    
+    titulo = models.TextField(default='Alerta')
+    mensagem = models.TextField()
+    data_hora = models.DateTimeField(auto_now_add=True)
+    tipo_notificacao = models.CharField(max_length=50, choices=TIPOS_NOTIFICACAO)
+    usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notificacoes')
+    lida = models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return f"Notificação {self.get_tipo_notificacao_display()} para {self.usuario} em {self.data_hora}"
+
+    class Meta:
+        ordering = ['-data_hora']
