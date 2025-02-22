42ef0bc1e68e82dcc0e149a65537bc7b6338ac69
LcasFerrei <lucasferreiraceara@gmail.com>
2025-02-22 19:38:16 -0300

    Ajustes de Rotas e Instalações de arquivos

--- AcquaSenseMobile/App.js
+++ AcquaSenseMobile/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Inicio from "./pages/Inicio";
+import Inicio2 from "./pages/Inicio2";
+import Inicio3 from "./pages/Inicio3";
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inicio" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Inicio" component={Inicio} />
+        <Stack.Screen name="Inicio2" component={Inicio2} />
+        <Stack.Screen name="Inicio3" component={Inicio3} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
