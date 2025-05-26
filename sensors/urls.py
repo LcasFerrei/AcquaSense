427a559a3713332ed15e8e3cabb8e5cc316e4d20
jardianagalvao <jardianagalvao@gmail.com>
2025-05-26 20:22:48 -0300

    Padronizar todas as views de notificações utilizando Django Rest Framework

--- sensors/urls.py
+++ sensors/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('relatorio-consumo/', views.consumo_relatorio, name='historico_consumo'),
     path('relatorio-consumo-semanal/', views.consumo_semanal, name='historico_consumo_semanal'),
     path('consumo-diario/', views.consumo_diario, name='consumo-diario'),
+    path('consumo-ponto-uso/', views.consumo_ponto_uso, name='consumo-ponto-uso'),
 ]
